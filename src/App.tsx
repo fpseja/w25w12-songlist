5feaecbe6985fa7989c2ec1a95d8a2f16c77d8c7
fpseja <5fpseja@gmail.com>
2025-11-20 18:31:14 +0900

    ✨TanStack Query사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import SongList from './pages/SongList.tsx'
 import SongDetail from './pages/SongDetail.tsx'
 import { getSongList } from './api/songApi.ts'
-import type { Song } from './types/Song.ts'
 
 function App() {
-  const [songs, setSongs] = useState<Song[]>([])
+  const { data: songs, isLoading, isError, error } = useQuery({
+    queryKey: ['songs'],
+    queryFn: getSongList
+  })
 
-  useEffect(() => {
-    const fetchSongs = async () => {
-      try {
-        const data = await getSongList()
-        setSongs(data)
-      } catch (err) {
-        console.error("Failed to fetch songs:", err)
-      }
-    }
-
-    fetchSongs()
-  }, [])
+  if (isLoading) {
+    return <p className="text-center mt-10">로딩 중...</p>
+  }
+  if (isError) {
+    return <p className="text-center mt-10">오류 발생: {error.message}</p>
+  }
 
   return (
     <Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter } from 'react-router-dom'
+
+const queryClient = new QueryClient()
 
 import './index.css'
 import App from './App.tsx'
@@ -11,7 +15,9 @@ const root = createRoot(container);
 root.render(
   <StrictMode>
     <HashRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </HashRouter>
   </StrictMode>
 );
